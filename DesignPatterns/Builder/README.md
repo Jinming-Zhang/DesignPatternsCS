2bf493008b39c31a7b1221ebf17f7fb049d32b8e
Jinming-Zhang <327429979@qq.com>
2020-08-24 03:14:43 -0400

    finished coding for fluent builder with inheritant, need more doc

--- DesignPatterns/Builder/README.md
+++ DesignPatterns/Builder/README.md
@@ -1,2 +1,10 @@
 # Builder Design Pattern
+
 Builder design pattern is used when the objects we want to build are kind of complecated, ie, has a lot of fields.
+
+## Fluent Builder
+
+## Fluent Builder with Inheritance
+
+The problem with inheriting in basic Fluent Builder is that the parent-builder will not have the information of the methods of their sub-builder, and so we cannot chain the methods in all the builders together.
+To solve this, we can use recursive generic types to provide more type information when creating the parent builder, and enables the ability to call methods in its sub-builder.
--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using Builder;
 using DelegateDemo;
+using FluentBuilderDemo;
+using FluentBuilderInheritanceDemo;
 namespace DesignPatternsC_
 {
     class Program
     {
         static void Main(string[] args)
         {
-            new DelegateDemo.DelegateDemo().Demo();
+            // new DelegateDemo.DelegateDemo().Demo();
             //%%%%%%%%%%%%%%% Builder Demos %%%%%%%%%%%%%%%%%%//
             // new Builder.HTMLStringBuilder().Demo();
             // new Builder.HTMLBuilder().Demo();
+            // new WolfFluentBuilderDemo().Demo();
+            new FluentBuilderInheritanceDemo.FluentBulderInheritanceDemo().Demo();
         }
     }
 }
