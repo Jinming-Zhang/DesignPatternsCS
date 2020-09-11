ce71d2e338346d5bfa3f769e8dca5b2aaeac468c
Jinming-Zhang <327429979@qq.com>
2020-09-11 12:27:32 -0400

    rawly finished the course

--- Program.cs
+++ Program.cs
@@ -1,95 +1,91 @@
 ﻿using System;
-using DelegateDemo;
 using AsyncDemo;
+using DelegateDemo;
+//************** Creational Design Patterns **************//
 // builder demos
-using SimpleBuilderDemo;
+using BuilderExcerciseDemo;
 using FacetedBuilderDemo;
 using FluentBuilderDemo;
 using FluentBuilderInheritanceDemo;
 using FunctionalBuilderDemo;
 using FunctionalBuilderGeneralized;
-using BuilderExcerciseDemo;
+using SimpleBuilderDemo;
 // Factory demos
-using FactoryMethodDemo;
-using AsyncFactoryMethodDemo;
-using AbstractFactoryDemo;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using AbstractFactoryDemo;
+using AsyncFactoryMethodDemo;
+using FactoryMethodDemo;
 // Prototype
 using static SimplePrototype.SimplePrototype;
 using static SerializationPrototype.SerializationPrototype;
-namespace DesignPatternsC_
-{
-    static class Test
-    {
-        public static string GetPasscode(string keypad, string instructions)
-        {
+// Singleton
+//************** Structural Design Patterns **************//
+// Adapter
+using static AdapterExercise.AdapterExercise;
+// Bridge
+using static BridgeExercise.BridgeExercise;
+// Composite
+// Decorator
+// Facade
+// Flyweight
+using static FlyweightExercise.FlyweightExercise;
+// Proxy
+//************** Behavior Design Patterns **************//
+// Chain of responsibility
+namespace DesignPatternsC_ {
+    static class Test {
+        public static string GetPasscode (string keypad, string instructions) {
             // keypad never empty or null
             string passcode = "";
             // todo
             // parse keypad into 2d arrays
-            string[] keypadRows = keypad.Split("\n");
-            List<string[]> keypadMatrix = new List<string[]>();
-            foreach (string r in keypadRows)
-            {
-                keypadMatrix.Add(r.Split(" "));
-                Console.WriteLine(r);
+            string[] keypadRows = keypad.Split ("\n");
+            List<string[]> keypadMatrix = new List<string[]> ();
+            foreach (string r in keypadRows) {
+                keypadMatrix.Add (r.Split (" "));
+                Console.WriteLine (r);
             }
             // find the index of first instruction
-            string begin = instructions[0].ToString();
-            int[] index = new int[] { -1, -1 };
-            for (int i = 0; i < keypadMatrix.Count; i++)
-            {
+            string begin = instructions[0].ToString ();
+            int[] index = new int[] {-1, -1 };
+            for (int i = 0; i < keypadMatrix.Count; i++) {
                 string[] row = keypadMatrix[i];
-                for (int j = 0; j < row.Length; j++)
-                {
-                    if (row[j] == begin)
-                    {
+                for (int j = 0; j < row.Length; j++) {
+                    if (row[j] == begin) {
                         index[0] = i;
                         index[1] = j;
                         break;
                     }
                 }
             }
-            Console.WriteLine($"key begins at: {index[0]}, {index[1]}");
+            Console.WriteLine ($"key begins at: {index[0]}, {index[1]}");
             // walk through instructions
-            string[] instructionArray = instructions.Split("\n");
+            string[] instructionArray = instructions.Split ("\n");
             // skip the first initial character
-            for (int i = 1; i < instructionArray.Length; i++)
-            {
+            for (int i = 1; i < instructionArray.Length; i++) {
                 // for each series of instructions
 
                 // walk through each instruction
                 int[] nextIndex = new int[] { index[0], index[1] };
-                foreach (char ins in instructionArray[i])
-                {
+                foreach (char ins in instructionArray[i]) {
                     int[] oldIndex = new int[] { nextIndex[0], nextIndex[1] };
-                    Console.WriteLine(ins);
+                    Console.WriteLine (ins);
                     // operations
-                    if (ins == 'U')
-                    {
+                    if (ins == 'U') {
                         nextIndex[0] -= 1;
-                    }
-                    else if (ins == 'D')
-                    {
+                    } else if (ins == 'D') {
                         nextIndex[0] += 1;
-                    }
-                    else if (ins == 'L')
-                    {
+                    } else if (ins == 'L') {
                         nextIndex[1] -= 1;
-                    }
-                    else if (ins == 'R')
-                    {
+                    } else if (ins == 'R') {
                         nextIndex[1] += 1;
                     }
                     // validate if still inside board
-                    if (nextIndex[0] >= 0 && nextIndex[0] <= keypadMatrix.Count && nextIndex[1] >= 0 && nextIndex[1] <= keypadMatrix[nextIndex[0]].Length)
-                    {
+                    if (nextIndex[0] >= 0 && nextIndex[0] <= keypadMatrix.Count && nextIndex[1] >= 0 && nextIndex[1] <= keypadMatrix[nextIndex[0]].Length) {
 
-                    }
-                    else
-                    {
+                    } else {
                         nextIndex[0] = oldIndex[0];
                         nextIndex[1] = oldIndex[1];
                     }
@@ -97,16 +93,14 @@ namespace DesignPatternsC_
                 index[0] = nextIndex[0];
                 index[1] = nextIndex[1];
                 passcode += keypadMatrix[index[0]][index[1]];
-                Console.WriteLine($"passcode is {passcode}");
-                Console.WriteLine("next");
+                Console.WriteLine ($"passcode is {passcode}");
+                Console.WriteLine ("next");
             }
             return passcode;
         }
     }
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main (string[] args) {
             // new DelegateDemo.DelegateDemo().Demo();
             // await AsyncDemo.AsyncDemo.Demo();
             //%%%%%%%%%%%%%%% Builder Demos %%%%%%%%%%%%%%%%%%//
@@ -126,6 +120,12 @@ namespace DesignPatternsC_
             //%%%%%%%%%%%%%%  Prototype  %%%%%%%%%%%%%%%%%%%%%//
             // SimplePrototypeDemo();
             // SerializationPrototypeDemo();
+            //%%%%%%%%%%%%%%  Adapter  %%%%%%%%%%%%%%%%%%%%%//
+            // AdapterExerciseDemo ();
+            //%%%%%%%%%%%%%%  Bridge  %%%%%%%%%%%%%%%%%%%%%//
+            // BridgeExerciseDemo ();
+            //%%%%%%%%%%%%%%  Flyweight  %%%%%%%%%%%%%%%%%%%%%//
+            FlyweightExerciseDemo();
         }
 
     }
